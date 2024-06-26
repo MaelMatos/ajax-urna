53b22a94e40f945da600b1a9cf708e5d7428312a
MaelMatos <145067851+MaelMatos@users.noreply.github.com>
2024-06-26 14:47:14 -0300

    Create apura.php

---
+++ apura.php
@@ -0,0 +1,97 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Painel de Candidatos</title>
+   <?php include "head.php"; ?>
+	<style>
+		 .cabecalho{
+		 	background: teal;
+            position: sticky; 
+            z-index: 999px;  
+            top: 0;
+            left: 0;
+            width: auto;            
+            color: #FFFFFF;
+            text-shadow: 4px 4px 4px black;
+            box-shadow: 4px 4px 4px black;
+            justify-content: space-between;
+		 }
+
+		.logo{
+            float: left;
+            width: 100px;
+            height: 100px;
+            border-radius: 24px;
+            padding: 5px;
+
+		}
+
+		.fotocandidato{
+			width: 120px;
+			height: 120px;
+		}
+
+		.fotocandidato:hover{
+            cursor: pointer;
+
+		}
+
+		.numerocandidato{
+			font-size: 30px;
+			color: black;
+		}
+
+		table{
+			background-color: white;
+			margin-left: 10px;
+
+		}
+	</style>
+</head>
+<body>
+ <div class="cabecalho">
+ 	<img src="logoetec.png" class="logo">
+ 	 <center>
+     <h1>Urna Eletrônica</h1>
+     <h2><i>Sistema de Votação online</i></h2>
+     </center>
+ </div>
+    <table style="width:100%;">
+      <tr>
+<?php
+require "head.php";
+$dados =  $con->query("select * from candidatos order by numero")->fetchAll(PDO::FETCH_ASSOC);
+$i = 0;
+foreach ($dados as $dado){
+   $foto = $dado['foto'];
+   $numero = $dado['numero'];
+   $nome = $dado['nome']; 
+   $votos = $con->query("select count(id) from votos where candidato=$numero")->fetch(PDO::FETCH_ASSOC);
+   $votos = $votos['id'];
+   if($i==3){
+      echo "</tr><tr>";
+      $i = 0;
+   }
+echo "<td><center><img class='fotocandidato' src='$foto'><br> <label class='numerocandidato'>$numero<br>$nome<br>votos:$votos</label></center></td>";
+$i = $i+1;
+
+}
+
+?>
+ </table>
+</div>
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
